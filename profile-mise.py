6237426ffb7c82632df6bc46e2a0c2068273f427
cascafico <cascafico@gmail.com>
2018-09-18 10:37:39 +0200

    conflation re-run

--- profile-mise.py
+++ profile-mise.py
@@ -1,13 +1,11 @@
 # value for OSM tag source
-source = 'Mise'
-#add_source = True
+source = 'MiseGas'
+add_source = True
 
 # do not add unique reference IDs to OSM?
 
-#no_dataset_id = True
-#no_dataset_id = False poipoi
-no_dataset_id = True
-#dataset_id = 'mise'
+no_dataset_id = False
+dataset_id = 'misegas'
 
 # Overpass query to use when searching OSM for data
 #overpass_timeout = 120 default
@@ -30,12 +28,9 @@ master_tags = ('fuel:lpg', 'fuel:cng')
 #master_tags = ('fuel:lpg')
 
 delete_unmatched = False
-#delete_unmatched = True
-#tag_unmatched = { 
-#'fixme':'This object might have been dismantled, please check' 
-#'amenity':None,
-#'disused:amenity':'fuel'
-#}
+tag_unmatched = { 
+'fixme':'This object might have been dismantled, please check' 
+}
 
 
 # max distance to search for a match in meters
